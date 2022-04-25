a937becef1bda6ad82599fffd876aff92b3f2b3d
Kshitij Chauhan (TJ) <tjchauhan2019@berkeley.edu>
2022-04-25 22:08:01 +0000

    updated tests

--- ligotools/tests/test_readligo.py
+++ ligotools/tests/test_readligo.py
@@ -3,26 +3,26 @@ from ligotools import readligo as rl
 import json
 
 def test_read_frame():
-    assert len(rl.loaddata(json.load(open("BBH_events_v3.json","r"))['GW150914']['fn_H1'],'H1'[1])) == 3 
+    assert len(rl.loaddata('data/'+json.load(open("data/BBH_events_v3.json","r"))['GW150914']['fn_H1'],'H1'[1])) == 3 
 
 def test__loaddata():
-    assert len(rl.loaddata(json.load(open("BBH_events_v3.json","r"))['GW150914']['fn_H1'],'H1')[2]) == 13
+    assert len(rl.loaddata('data/'+json.load(open("data/BBH_events_v3.json","r"))['GW150914']['fn_H1'],'H1')[2]) == 13
 
 def test_dq2segst():
-    assert type(rl.loaddata(json.load(open("BBH_events_v3.json","r"))['GW150914']['fn_H1'],'H1')[2]) == dict
+    assert type(rl.loaddata('data/'+json.load(open("data/BBH_events_v3.json","r"))['GW150914']['fn_H1'],'H1')[2]) == dict
 
 def test_dq_channel_to_seglist():
-    assert type(rl.loaddata(json.load(open("BBH_events_v3.json","r"))['GW150914']['fn_H1'],'H1')[2]) == dict
+    assert type(rl.loaddata('data/'+json.load(open("data/BBH_events_v3.json","r"))['GW150914']['fn_H1'],'H1')[2]) == dict
 
 def test_whiten():
-    assert len(rl.loaddata(json.load(open("BBH_events_v3.json","r"))['GW150914']['fn_H1'],'H1')[0]) == 131072
+    assert len(rl.loaddata('data/'+json.load(open("data/BBH_events_v3.json","r"))['GW150914']['fn_H1'],'H1')[0]) == 131072
 
 def test_write_wavfile():
-    assert type(rl.loaddata(json.load(open("BBH_events_v3.json","r"))['GW150914']['fn_H1'],'H1')[0]) == numpy.ndarray
+    assert type(rl.loaddata('data/'+json.load(open("data/BBH_events_v3.json","r"))['GW150914']['fn_H1'],'H1')[0]) == numpy.ndarray
 
 def test_reqshift():
-    assert type(rl.loaddata(json.load(open("BBH_events_v3.json","r"))['GW150914']['fn_H1'],'H1')[0]) == numpy.ndarray
+    assert type(rl.loaddata('data/'+json.load(open("data/BBH_events_v3.json","r"))['GW150914']['fn_H1'],'H1')[0]) == numpy.ndarray
 
 def test_plotting_results():
-    assert len(rl.loaddata(json.load(open("BBH_events_v3.json","r"))['GW150914']['fn_H1'],'H1')[1]) == 131072
+    assert len(rl.loaddata('data/'+json.load(open("data/BBH_events_v3.json","r"))['GW150914']['fn_H1'],'H1')[1]) == 131072
     
